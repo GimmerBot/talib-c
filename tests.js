dfe5a3f4e7513a3da9a6212b11a67c39436e5666
Persio Arco e Flexa <Persio Arco e Flexa>
2016-12-24 11:20:21 -0200

    include dlls

--- tests.js
+++ tests.js
@@ -1,4 +1,4 @@
-var tablib = require("./index.js");
+var talib = require("./index.js");
 
-tablib.macd(0, 0, [12,3,45], 3, 4, 5, [12,3,45], [12,3,45], [12,3,45], function(result){    
+talib.macd(0, 0, [12,3,45], 3, 4, 5, [12,3,45], [12,3,45], [12,3,45], function(result){    
 });
